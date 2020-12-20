93e31c1dc965a769caa9a29209cc3920dfddd0bc
Nathan <none>
2020-12-20 16:39:30 -0500

    bump serial to 250k, keep display at 115k

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -997,7 +997,7 @@ void setup() {
   uint32_t serial_connect_timeout = millis() + 1000UL;
   while (!MYSERIAL0 && PENDING(millis(), serial_connect_timeout)) { /*nada*/ }
   #if HAS_MULTI_SERIAL && !HAS_ETHERNET
-    MYSERIAL1.begin(BAUDRATE);
+    MYSERIAL1.begin(115200);
     serial_connect_timeout = millis() + 1000UL;
     while (!MYSERIAL1 && PENDING(millis(), serial_connect_timeout)) { /*nada*/ }
   #endif
